e8377064063b630028a3a54fe689087d2a10210e
Osha G <ogroetz@gmail.com>
2020-04-16 00:08:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+
+  new_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
+  def badge_maker(name)
+    "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(names)
+    badge_messages = []
+    badge.each do |name|
+      
