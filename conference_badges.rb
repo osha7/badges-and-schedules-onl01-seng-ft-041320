952daa8faecee7f56474c0d3e6d40548fff8008e
Osha G <ogroetz@gmail.com>
2020-04-16 00:09:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,24 @@
-
-  new_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ new_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   
   def badge_maker(name)
     "Hello, my name is #{name}."
   end
   
-  def batch_badge_creator(names)
-    badge_messages = []
-    badge.each do |name|
+  def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |name|
+     badges << badge_maker(name)
+    end
+    return badges
+end
+      
+  def assign_rooms(names)
+    room_assignments = []
+    counter = 1
+    names.each_with_index do |name|
+     "Hello, #{name}! You'll be assigned to room #{counter}!"
+      counter =+ 1 
+    end
+    return room_assignments
+  end
       
